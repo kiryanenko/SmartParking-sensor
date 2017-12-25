e0c6869444ed369821f5a8a0e49b8138e34a7620
Kiryanenko Alexander <kiryanenkoav@gmail.com>
2017-12-25 22:50:43 +0300

    Set parameters

--- Parameters.cpp
+++ Parameters.cpp
@@ -31,6 +31,26 @@ uint16_t Parameters::getSendingPeriod() const
 	return m_sendingPeriod;
 }
 
+uint16_t Parameters::getDayCost() const
+{
+	return m_dayCost;
+}
+
+uint16_t Parameters::getNightCost() const
+{
+	return m_nightCost;
+}
+
+uint16_t Parameters::getDayStartTime() const
+{
+	return m_dayStartTime;
+}
+
+uint16_t Parameters::getNightStartTime() const
+{
+	return m_nightStartTime;
+}
+
 void Parameters::setId(const uint32_t id)
 {
 	if (m_id != id) {
@@ -53,4 +73,32 @@ void Parameters::setSensorSamplingPeriod(const uint16_t samplingPeriod)
 		m_sensorSamplingPeriod = samplingPeriod;
 		EEPROM.write(adress_sensor_sampling_period, samplingPeriod);
 	}
+}
+
+void Parameters::setDayCost(uint16_t cost)
+{
+	if (m_dayCost != cost) {
+		m_dayCost = cost;
+		EEPROM.write(adress_id, cost);
+	}
+}
+
+void Parameters::setNightCost(uint16_t cost)
+{
+	if (m_nightCost != cost) {
+		m_nightCost = cost;
+		EEPROM.write(adress_id, cost);
+	}
+}
+
+void Parameters::setDayStartTime(uint16_t time)
+{
+	if (m_dayStartTime != time) {
+		m_dayStartTime = time;
+		EEPROM.write(adress_id, cost);
+	}
+}
+
+void Parameters::setNightStartTime(uint16_t time)
+{
 }
